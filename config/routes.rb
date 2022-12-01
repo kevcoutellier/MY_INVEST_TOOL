51d17f2be280499309b527fb71030d4cfd3113da
Kevin Coutellier <kev.coutellier@gmail.com>
2022-12-01 17:51:01 +0100

    API crypto presque terminé

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   get 'wallet', to: "pages#wallet"
   resources :pages, only: [:index]
-  resources :cryptos, only: [:new, :index, :destroy, :edit, :update, :show]
+  resources :cryptos
   resources :bank_accounts, only: [:new, :index, :destroy, :edit]
-  resources :real_estates, only: [:new, :index, :destroy, :edit, :show, :create, :update]
+  resources :real_estates
   resources :custom_invests, only: [:new, :index, :destroy, :edit]
   resources :liabilities, only: [:new, :index, :destroy, :edit]
 
