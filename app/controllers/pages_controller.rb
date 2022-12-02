a2f48af05c547e723318234f01b1bbfd9556519e
diana spieser <dianaspieser@gmail.com>
2022-12-02 09:54:45 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :dashboard ]
 
   def dashboard
+    @user = current_user
+    @real_estates = RealEstate.all
+
   end
 
   def wallet
