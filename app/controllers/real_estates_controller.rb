eefb86f85ca9e2bba50d35521bd52e7819f4ea15
diana spieser <dianaspieser@gmail.com>
2022-12-06 11:44:36 +0100

    ok

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -16,6 +16,24 @@ class RealEstatesController < ApplicationController
       }
     end
   end
+  def estimation(address)
+    encoded_address = address.gsub(",","%2C").gsub(" ","%20").gsub("É","%C3%89")
+
+    # model = "400%20Ocean%20Dr%2C%20Miami%20Beach%2C%20FL%2033139%2C%20%C3%89tats-Unis"
+    url = URI("https://realty-mole-property-api.p.rapidapi.com/salePrice?address=#{encoded_address}&propertyType=Single%20Family&bedrooms=4&bathrooms=2&squareFootage=1600&compCount=5")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = 'f835ac6c59msh54715db3f512f86p19a480jsnf14aac81f626'
+    request["X-RapidAPI-Host"] = 'realty-mole-property-api.p.rapidapi.com'
+
+    response = http.request(request)
+    @response = response.read_body
+    @response = JSON.parse(response.read_body)
+    @response = @response["price"]
+  end
 
   def new
     @real_estate = RealEstate.new
@@ -27,6 +45,7 @@ class RealEstatesController < ApplicationController
 
   def create
     @real_estate = RealEstate.new(real_estate_params)
+    @real_estate.estimation = estimation(@real_estate.address)
     @real_estate.user = current_user
     if @real_estate.save
       redirect_to real_estate_path(@real_estate)
@@ -40,6 +59,7 @@ class RealEstatesController < ApplicationController
 
   def update
     if @real_estate.update(real_estate_params)
+      @real_estate.estimation = estimation(@real_estate.address)
       redirect_to real_estates_path
     else
       render :edit, status: :unprocessable_entity
