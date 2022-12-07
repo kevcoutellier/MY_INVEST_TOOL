540e8ffbfe0bf409d851b35f8871d972e9c5d21e
diana spieser <dianaspieser@gmail.com>
2022-12-07 11:05:26 +0100

    changement fabrice

--- app/javascript/components/swiper_card.js
+++ app/javascript/components/swiper_card.js
@@ -1,20 +1,26 @@
 
 export const swiper_card = () => {
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
+  const swipCard = document.querySelector('.swiper')
 
-  // pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  if (swipCard) {
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+
+      // pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
 }
