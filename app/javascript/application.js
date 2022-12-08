0dae38e8b40586d5e6cbd0ea1b296305d6873ec7
Teddy Muller <muller.teddy@icloud.com>
2022-12-08 10:06:16 +0100

    prob ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,21 +2,22 @@
 // import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
+// import Rails from "@rails/ujs";
+// import Turbolinks from "turbolinks";
+// import * as ActiveStorage from "@rails/activestorage";
 
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+// Rails.start();
+// Turbolinks.start();
+// ActiveStorage.start();
 // import { swiperCard } from "./components/swiper_card"
 
 import { navbar } from "./components/navbar"
 import { donut } from "./components/chart"
 import { bars } from "./components/chart2"
 import { swiper_card } from "./components/swiper_card";
-
-document.addEventListener("turbolinks:load", function () {
+// turbolinks:load
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('donut entry');
   swiper_card();
   navbar();
   donut();
